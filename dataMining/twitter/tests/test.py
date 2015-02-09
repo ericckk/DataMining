439b8033ca7d0ae7619ed6102800f4024062a8e1
justin milanovic <justinmilanovic@gmail.com>
2015-02-08 23:08:49 -0700

    upload mongo file

---
+++ dataMining/twitter/tests/test.py
@@ -0,0 +1,14 @@
+
+#! /usr/bin/env python
+
+__author__ = "Justin Milanovic"
+__copyright__ = "Copyright 2015, HireGround"
+__version__ = "1.0.0"
+__email__ = "justinmilanovic@gmail.com"
+__status__ = "Development"
+
+
+if __name__ == '__main__':
+    pass
+
+ 
