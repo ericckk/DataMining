fa5d8209721bbc45e5d0487a6f56ec604cd3d580
justin milanovic <justinmilanovic@gmail.com>
2015-03-01 01:47:37 -0700

    unit tests

---
+++ dataMining/twitter/test/TestExtraction.py
@@ -0,0 +1,22 @@
+
+#! /usr/bin/env python
+
+__author__ = "Justin Milanovic"
+__copyright__ = "Copyright 2015, HireGround"
+__version__ = "1.0.0"
+__email__ = "justinmilanovic@gmail.com"
+__status__ = "Development"
+
+import unittest
+from dataMining.twitter.extraction import punctuation
+
+
+class TestExtractionFunctions(unittest.TestCase):
+    
+    def testPunctuation(self):
+        self.testString = ';Th:is ,is| a test.'
+        self.assertEqual("This is a test", punctuation(self.testString))
+        
+
+if __name__ == '__main__':
+    unittest.main()
